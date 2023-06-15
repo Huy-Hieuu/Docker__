ea6e14c67296e40be1fd368459cb3ba1d086782e
HuyHieu-19 <104619096+HuyHieu-19@users.noreply.github.com>
2023-06-15 17:06:37 +0700

    .

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -4,10 +4,18 @@ import { NavLink } from "react-router-dom";
 function Header(props) {
   return (
     <header className="header flex items-center justify-center gap-x-5 text-white py-10 mb-10">
-      <NavLink to={"/"} className={{} ? "" : ""}>
+      <NavLink
+        to={"/"}
+        // className={({ isActive }) => (isActive ? "text-red-600" : "")}
+      >
         Home
       </NavLink>
-      <NavLink to={"/movies"}>Movies</NavLink>
+      <NavLink
+        to={"/movies"}
+        // className={({ isActive }) => (isActive ? "text-red-600" : "")}
+      >
+        Movies
+      </NavLink>
     </header>
   );
 }
